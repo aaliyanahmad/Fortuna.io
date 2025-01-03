16efcf46eb97cb33b84b47bc478877bfea7d276e
A4LIYAN <107790546+aaliyanahmad@users.noreply.github.com>
2025-01-04 03:18:57 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all JS, JSX, TS, and TSX files in the src directory
+    "./public/index.html", // Include the index.html file in the public directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
